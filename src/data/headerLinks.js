1b76dc4b5f0f19f089852624cb7931a396b29225
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-02-28 18:33:40 +0100

    add folder data and comments

---
+++ src/data/headerLinks.js
@@ -0,0 +1,52 @@
+export default [
+        {
+          label: "CHARACTERS",
+          link: "#",
+          active: false
+        },
+        {
+          label: "COMICS",
+          link: "#",
+          active: false
+        },
+        {
+          label: "MOVIES",
+          link: "#",
+          active: false
+        },
+        {
+          label: "TV",
+          link: "#",
+          active: false
+        },
+        {
+          label: "GAMES",
+          link: "#",
+          active: false
+        },
+        {
+          label: "COLLECTIBLES",
+          link: "#",
+          active: false
+        },
+        {
+          label: "VIDEOS",
+          link: "#",
+          active: false
+        },
+        {
+          label: "FANS",
+          link: "#",
+          active: false
+        },
+        {
+          label: "NEWS",
+          link: "#",
+          active: false
+        },
+        {
+          label: "SHOP",
+          link: "#",
+          active: false
+        },
+      ]
---
+++ src/data/iconsListArray.js
@@ -0,0 +1,22 @@
+export default [
+        {
+          img: "comics-digital-comics.png",
+          text: "DIGITAL COMICS"
+        },
+        {
+          img: "comics-merchandise.png",
+          text: "DC MERCHANDISE"
+        },
+        {
+          img: "comics-subscriptions.png",
+          text: "SUBSCRIPTION"
+        },
+        {
+          img: "comics-shop-locator.png",
+          text: "COMIC SHOP LOCATOR"
+        },
+        {
+          img: "dc-power-visa.svg",
+          text: "DC POWER VISA"
+        },
+      ]
